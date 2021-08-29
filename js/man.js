04d9be5b392aa031ea7cfa9095217f021ccbfd4a
sajib <sajibshariful54@gmail.com>
2021-08-29 17:29:59 +0600

    sajib

---
+++ js/man.js
@@ -0,0 +1,86 @@
+document.getElementById('memory_button_8gb').addEventListener('click', function () {
+
+    const memory_button_8gb = 10;
+    //  console.log('sajib')
+    const memory_8gb = document.getElementById('memory_span');
+    console.log(memory_8gb.innerText = memory_button_8gb)
+    //const memory_span_16 = memory_16Gb.value;
+    //memory_16Gb.value = parseInt(memory_span_16) + 1;
+
+    //console.log('sajib')
+});
+
+
+// handle deposit button event
+document.getElementById('memory_button_16Gb').addEventListener('click', function () {
+
+    const memory_button_16Gb = 300;
+    //  console.log('sajib')
+    const memory_16Gb = document.getElementById('memory_span');
+    console.log(memory_16Gb.innerText = memory_button_16Gb)
+    //const memory_span_16 = memory_16Gb.value;
+    //memory_16Gb.value = parseInt(memory_span_16) + 1;
+
+    //console.log('sajib')
+});
+
+// memory work end
+
+document.getElementById('storage_button_256gb').addEventListener('click', function () {
+
+    const storage_button_256gb = 10;
+
+    const storage_256gb = document.getElementById('storage');
+    console.log(storage_256gb.innerText = storage_button_256gb)
+
+});
+
+document.getElementById('storage_button_512gb').addEventListener('click', function () {
+
+    const storage_button_512gb = 100;
+
+    const storage_512gb = document.getElementById('storage');
+    console.log(storage_512gb.innerText = storage_button_512gb)
+
+});
+document.getElementById('storage_button_1tb').addEventListener('click', function () {
+
+    const storage_button_1tb = 700;
+
+    const storage_1tb = document.getElementById('storage');
+    console.log(storage_1tb.innerText = storage_button_1tb)
+
+});
+
+//END Storage
+document.getElementById('delivery_free').addEventListener('click', function () {
+
+    const delivery_free = 500;
+
+    const delivery_output = document.getElementById('delivery_output');
+    console.log(delivery_output.innerText = delivery_free)
+
+});
+
+document.getElementById('delivery_free_2').addEventListener('click', function () {
+
+    const delivery_free_2 = 200;
+
+    const delivery_output = document.getElementById('delivery_output');
+    console.log(delivery_output.innerText = delivery_free_2)
+
+});
+
+
+function caltotal() {
+    const micbooktotal = document.getElementById('total')
+    const totalnum = delivery_output * delivery_output;
+}
+
+/////////////////////////sorry total ta korty pari ny
+
+
+
+
+
+
